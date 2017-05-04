e5f591f0e11ebf5bc5a7c90d49f01f5170493eec
Marcos Segovia <velozmarkdrea@gmail.com>
2017-05-04 14:02:26 +0200

    Adding DateTimeFormatStrategy and JsonSerializableStrategy to Common Strategies

---
+++ src/Strategy/DateTimeFormatStrategy.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace Uvinum\Strategy;
+
+final class DateTimeFormatStrategy implements Strategy
+{
+    /** @var Strategy | null */
+    private $nextStrategy;
+
+    public function __construct($nextStrategy = null)
+    {
+        $this->nextStrategy = $nextStrategy;
+    }
+
+    public function execute($object)
+    {
+        if ($object instanceof \DateTimeInterface) {
+            return $object->format('Y-m-d H:i:s');
+        }
+
+        return $this->next($object);
+    }
+
+    public function next($object)
+    {
+        if (null !== $this->nextStrategy) {
+            return $this->nextStrategy->execute($object);
+        }
+
+        return null;
+    }
+}
